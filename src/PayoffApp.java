e5484a6835cdce3528781d188ae7c9b81dcad9df
Viktor Huchiev <274128020+viktorHuc@users.noreply.github.com>
2026-04-20 11:04:36 -0700

    Added a List for aprs and sorted it

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,13 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class PayoffApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        List<Double> aprs = new ArrayList<>();
+
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
 
             double apr = scan.nextDouble();
+            aprs.add(apr);
             double balance = scan.nextDouble();
 
             // Consume \n after balance input 
@@ -17,5 +24,7 @@ public class PayoffApp {
             String balanceString = String.format("$%.2f", balance);
             System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
         }
+        Collections.sort(aprs, Collections.reverseOrder());
+        System.out.println(aprs);
     }
 }
